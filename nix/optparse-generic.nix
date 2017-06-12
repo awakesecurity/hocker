bd0468478de501a8198c2090082d9d75a4d14773
Parnell Springmeyer <parnell@digitalmentat.com>
2017-06-11 21:06:20 -0700

    Switch to the hackage version

--- nix/optparse-generic.nix
+++ nix/optparse-generic.nix
@@ -1,15 +1,10 @@
-{ mkDerivation, base, bytestring, fetchgit, optparse-applicative
-, semigroups, stdenv, system-filepath, text, time, transformers
-, void
+{ mkDerivation, base, bytestring, optparse-applicative, semigroups
+, stdenv, system-filepath, text, time, transformers, void
 }:
 mkDerivation {
   pname = "optparse-generic";
   version = "1.2.0";
-  src = fetchgit {
-    url = "https://github.com/Gabriel439/Haskell-Optparse-Generic-Library";
-    sha256 = "0dy7z16287pf26bjxams8xz4i6is34l2a5qzpsqgap93i3s8p256";
-    rev = "f8449f420b5595001128a80282538bdbd75bf030";
-  };
+  sha256 = "1zhvrwcrchnls5fhy4m0466hhz84whrc2xm7nxwk1xjnmaqrb4l7";
   libraryHaskellDepends = [
     base bytestring optparse-applicative semigroups system-filepath
     text time transformers void
