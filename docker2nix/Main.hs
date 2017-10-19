8269dabf7540f6d0a73d5c177dc6a65871969c9c
Parnell Springmeyer <parnell@digitalmentat.com>
2017-10-19 18:28:44 -0500

    Ignore unused-do-bind

--- docker2nix/Main.hs
+++ docker2nix/Main.hs
@@ -8,7 +8,9 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StandaloneDeriving  #-}
 {-# LANGUAGE TypeOperators       #-}
-{-# OPTIONS -fno-warn-orphans    #-}
+
+{-# OPTIONS -fno-warn-orphans        #-}
+{-# OPTIONS -fno-warn-unused-do-bind #-}
 
 -----------------------------------------------------------------------------
 -- |
