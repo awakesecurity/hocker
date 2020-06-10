7e9c5563e7aaa7360078d99da766a95ee5401824
John C. Carey <jcarey@awakesecurity.com>
2020-06-09 17:13:01 -0700

    Changes suggested by @evalrelf.

--- nix/fetchNixpkgs.nix
+++ nix/fetchNixpkgs.nix
@@ -1,6 +1,5 @@
 { rev                             # The Git revision of nixpkgs to fetch
 , sha256                          # The SHA256 hash of the unpacked archive
-, system ? builtins.currentSystem # This is overridable if necessary
 }:
 
 # In Nix 1.12, we can just give a `sha256` to `builtins.fetchTarball`.
