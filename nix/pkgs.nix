f27b5de31fb9b7ba1dcbea374994a72edc8e025e
Tomas Drtina <tdrtina-ext@arista.com>
2025-04-23 16:35:35 +0200

    nixpkgs: bump, fix mtl incompatibility

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -1,8 +1,8 @@
 let
-  # master as of 2022-05-16
-  rev = "72cb3780a57881aa2fc5e2268380d85de7ea2b4c";
+  # "nixos-unstable" as on 2024-12-02
+  rev = "ac35b104800bff9028425fec3b6e8a41de2bbfff";
 
-  sha256 = "sha256-NNuTF0Z2jHErDr0JLXd+h996xPXGS9cbBBJDENUf13o=";
+  sha256 = "sha256:1fbj7shlmviilmgz5z2gp59j6xwgdr01jfh75qhixx06kib4305p";
 
   nixpkgs = builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
